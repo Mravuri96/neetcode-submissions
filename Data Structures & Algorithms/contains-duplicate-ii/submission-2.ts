d1d203a3f2a836a3463b57a2bf05886b6a124164
Mahesh Ravuri <mravuri96@gmail.com>
2026-08-24 13:31:10 -0700

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {boolean}
+     */
+    // Author Mravuri96
+    containsNearbyDuplicate(nums: number[], k: number): boolean {
+        let [i, seen] = [0, new Set()];
+
+        while (i < nums.length) {
+            const el = nums[i];
+            if (seen.has(el)) return true;
+            seen.add(el);
+            if (i >= k) seen.delete(nums[i - k]);
+            i++;
+        }
+
+        return false;
+    }
+}
